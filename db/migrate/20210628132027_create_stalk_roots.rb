75e95d0f8ab9b30d5b6cb6c08bff72350ed741c1
manaka <romeod@gmail.com>
2021-06-28 15:20:45 +0200

    added StalkRoot dictionary

---
+++ db/migrate/20210628132027_create_stalk_roots.rb
@@ -0,0 +1,10 @@
+class CreateStalkRoots < ActiveRecord::Migration[6.1]
+  def change
+    create_table :stalk_roots do |t|
+      t.string :name
+      t.string :abbr
+
+      t.timestamps
+    end
+  end
+end
