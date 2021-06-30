3993c7255507297e505e5d0f5e2df219618cc73f
manaka <romeod@gmail.com>
2021-06-30 16:25:07 +0200

    Moving common types to separate file

---
+++ app/javascript/packs/types.ts
@@ -0,0 +1,29 @@
+export type DictCommonType = {
+    id: number,
+    name: string
+}
+export type DictType = {
+    cap_shapes: Array<DictCommonType>
+    cap_surfaces: Array<DictCommonType>
+    colors: Array<DictCommonType>
+    odors: Array<DictCommonType>
+    gill_attachments: Array<DictCommonType>
+    gill_spacings: Array<DictCommonType>
+    gill_sizes: Array<DictCommonType>
+}
+
+export type MushroomType = {
+    id: number,
+    name: string
+    edible: boolean
+    cap_shape_id: number
+    cap_surface_id: number
+    cap_color_id: number
+    bruises: boolean
+    odor_id: number
+    gill_attachment_id: number
+    gill_spacing_id: number
+    gill_size_id: number
+
+}
+
