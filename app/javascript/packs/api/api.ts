09963f02832f1785f450a0ef2d333282452d48f8
manaka <romeod@gmail.com>
2021-07-02 15:36:11 +0200

    switching api to production

--- app/javascript/packs/api/api.ts
+++ app/javascript/packs/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import {DictType, MushroomType} from "../types";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api/v1/',
+    baseURL: 'https://mushrooms.manaka.dev/',
 });
 
 type APIDictionariesType = {
