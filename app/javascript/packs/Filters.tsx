265f83dac5158e048cea8f8844f4d9324f84f38b
manaka <romeod@gmail.com>
2021-07-02 15:11:34 +0200

    Added more filters

--- app/javascript/packs/Filters.tsx
+++ app/javascript/packs/Filters.tsx
@@ -23,11 +23,26 @@ const Filters:FC<PropsType> = (props) => {
 
     const classes = props.classes
 
-    const capShapeChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    const selectChanged = (event: React.ChangeEvent<{ value: unknown }>) => {
         props.filterChanged(1, {cap_shape_id: event.target.value as number })
     };
     const capSurfaceChange = (event: React.ChangeEvent<{ value: unknown }>) => {
         props.filterChanged(1, {cap_surface_id: event.target.value as number })
+    };
+    const capColorChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        props.filterChanged(1, {cap_color_id: event.target.value as number })
+    };
+    const odorChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        props.filterChanged(1, {odor_id: event.target.value as number })
+    };
+    const gillAttachmentChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        props.filterChanged(1, {gill_attachment_id: event.target.value as number })
+    };
+    const gillSpacingChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        props.filterChanged(1, {gill_spacing_id: event.target.value as number })
+    };
+    const gillSizeChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        props.filterChanged(1, {gill_size_id: event.target.value as number })
     };
 
     if (props.dictionaries.size === 0) return <div>Loading</div>
@@ -46,14 +61,36 @@ const Filters:FC<PropsType> = (props) => {
             <List>
                 <ListItem button key={'cap_shape'}>
                     <BaseSelector dictionary={props.dictionaries.cap_shapes} name={'cap_shape_id'} humanName={'Cap Shape'}
-                                  value={props.filters.cup_shape_id} handleChange={capShapeChange}/>
+                                  value={props.filters.cup_shape_id} handleChange={selectChanged}/>
                 </ListItem>
-                <ListItem button key={'capShape2'}>
+                <ListItem button key={'cap_surfaces'}>
                     <BaseSelector dictionary={props.dictionaries.cap_surfaces} name={'cap_surface_id'} humanName={'Cap Surface'}
                                   value={props.filters.cup_surface_id} handleChange={capSurfaceChange}/>
 
                 </ListItem>
-                <ListItem button key={'capShape3'}>
+                <ListItem button key={'cap_color'}>
+                    <BaseSelector dictionary={props.dictionaries.colors} name={'cap_color_id'} humanName={'Cap Color'}
+                                  value={props.filters.cap_color_id} handleChange={capColorChange}/>
+
+                </ListItem>
+                <ListItem button key={'odor'}>
+                    <BaseSelector dictionary={props.dictionaries.odors} name={'odor_id'} humanName={'Odor'}
+                                  value={props.filters.odor_id} handleChange={odorChange}/>
+
+                </ListItem>
+                <ListItem button key={'gill_attachment'}>
+                    <BaseSelector dictionary={props.dictionaries.gill_attachments} name={'gill_attachment_id'} humanName={'Gill Attachment'}
+                                  value={props.filters.gill_attachment_id} handleChange={gillAttachmentChange}/>
+
+                </ListItem>
+                <ListItem button key={'gill_spacing'}>
+                    <BaseSelector dictionary={props.dictionaries.gill_spacings} name={'gill_spacing_id'} humanName={'Gill Spacing'}
+                                  value={props.filters.gill_spacing_id} handleChange={gillSpacingChange}/>
+
+                </ListItem>
+                <ListItem button key={'gill_size_id'}>
+                    <BaseSelector dictionary={props.dictionaries.gill_spacings} name={'gill_size_id'} humanName={'Gill Size'}
+                                  value={props.filters.gill_size_id} handleChange={gillSizeChange}/>
 
                 </ListItem>
 
